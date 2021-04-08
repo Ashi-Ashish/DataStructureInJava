fa2d585c3bd02a7d6d317ed41e86ad58e3292b46
AshishRalh <ashishstallion96@icloud.com>
2021-04-08 23:54:44 +0530

    LeetCode Arrays NumberOfGoodPairs Comments

--- src/com/LeetCode/Arrays/NumberOfGoodPairs.java
+++ src/com/LeetCode/Arrays/NumberOfGoodPairs.java
@@ -1,10 +1,16 @@
 package com.LeetCode.Arrays;
 
+//Given an array of integers nums.
+//A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+//Return the number of good pairs.
+//Example 1:
+//Input: nums = [1,2,3,1,1,3]
+//Output: 4
+//Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
 public class NumberOfGoodPairs {
     public static void main(String[] args){
-        int[] listOfNumbers = new int[]{1,2,3,1,1,3};
-        int totalIdenticalPairs = numIdenticalPairs(listOfNumbers);
-        System.out.println(totalIdenticalPairs);
+        System.out.println(numIdenticalPairs(new int[]{1,2,3,1,1,3}));
     }
 
     public static int numIdenticalPairs(int[] nums) {
