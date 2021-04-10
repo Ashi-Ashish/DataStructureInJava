e7b2c90d66ad052c571a883c61231f2d15617cc8
AshishRalh <ashishstallion96@icloud.com>
2021-04-11 00:46:20 +0530

    LeetCode Arrays XOROperationInArray

--- src/com/LeetCode/Arrays/XOROperationInArray.java
+++ src/com/LeetCode/Arrays/XOROperationInArray.java
@@ -1,14 +1,24 @@
 package com.LeetCode.Arrays;
 
+//Given an integer n and an integer start.
+//Define an array nums where nums[i] = start + 2*i (0-indexed) and n == nums.length.
+//Return the bitwise XOR of all elements of nums.
+//Example 1:
+//Input: n = 5, start = 0
+//Output: 8
+//Explanation: Array nums is equal to [0, 2, 4, 6, 8] where (0 ^ 2 ^ 4 ^ 6 ^ 8) = 8.
+//Where "^" corresponds to bitwise XOR operator.
+
 public class XOROperationInArray {
     public static void main(String[] args){
-
+        System.out.println(xorOperation(4, 3));
     }
 
     public static int xorOperation(int n, int start) {
-        int ans = start;
+        int xorAns = start;
         for (int i = 1; i < n; i++) {
-
+            xorAns = xorAns ^ (start + (2 * i));
         }
+        return xorAns;
     }
 }
