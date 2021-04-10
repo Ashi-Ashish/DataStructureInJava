ccd3bfb5c3e7cae35238476e00e9f7256ea1548e
AshishRalh <ashishstallion96@icloud.com>
2021-04-10 23:59:57 +0530

    LeetCode Arrays XOROperationInArray

---
+++ src/com/LeetCode/Arrays/XOROperationInArray.java
@@ -0,0 +1,14 @@
+package com.LeetCode.Arrays;
+
+public class XOROperationInArray {
+    public static void main(String[] args){
+
+    }
+
+    public static int xorOperation(int n, int start) {
+        int ans = start;
+        for (int i = 1; i < n; i++) {
+
+        }
+    }
+}
