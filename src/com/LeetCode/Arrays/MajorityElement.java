d11d1d17d17206e46ff6e486282372d4dbc2e3d4
AshishRalh <ashishstallion96@icloud.com>
2021-04-12 22:53:03 +0530

    LeetCode Arrays MajorityElement Summary

--- src/com/LeetCode/Arrays/MajorityElement.java
+++ src/com/LeetCode/Arrays/MajorityElement.java
@@ -4,6 +4,12 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
+//Given an array nums of size n, return the majority element.
+//The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+//Example 1:
+//Input: nums = [3,2,3]
+//Output: 3
+
 public class MajorityElement {
     public static void main(String[] args){
         System.out.println(majorityElement(new int[]{2,2,1,1,1,2,2}));
