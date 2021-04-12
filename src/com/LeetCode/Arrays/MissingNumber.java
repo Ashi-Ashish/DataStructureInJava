de820493fca024a37a01079921879aa78ccbe291
AshishRalh <ashishstallion96@icloud.com>
2021-04-12 23:58:51 +0530

    LeetCode Arrays MissingNumber

---
+++ src/com/LeetCode/Arrays/MissingNumber.java
@@ -0,0 +1,22 @@
+package com.LeetCode.Arrays;
+
+//Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+//Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+//Example 1:
+//Input: nums = [3,0,1]
+//Output: 2
+//Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+public class MissingNumber {
+
+    public static void main(String[] args){
+        System.out.println(missingNumber(new int[]{3,0,1}));
+    }
+
+    public static int missingNumber(int[] nums){
+        int expectedSum = nums.length*(nums.length + 1)/2;
+        int actualSum = 0;
+        for (int num : nums) actualSum += num;
+        return expectedSum - actualSum;
+    }
+}
